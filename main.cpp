3dcd72f9c7797b3c14f231465affbc4025fa41c8
petermdtran246 <petermdtran246@gmail.com>
2024-05-13 17:01:46 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+typedef double Item;
+
+int main()
+{
+   int a, b, c;
+   cout << "Enter two numbers: " << endl;
+   cin >> a >> b;
+   if(b==0) {
+       cout << "Invalid denominator" << endl;
+   }
+   else {
+       c = a/b;
+       cout << c << endl;
+   }
+
+    return 0;
+}
